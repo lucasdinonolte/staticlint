91bf14bf7db4cb2bb8b0a7120bd432155e7ffb6c
Lucas Nolte <lucas@designsystems.international>
2022-09-19 21:40:33 +0200

    chore: format eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,18 @@
 module.exports = {
-    'env': {
-        'node': true,
-        'es2021': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ],
-        'comma-dangle': [
-          'error', 'always-multiline',
-        ],
-    }
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
 }
